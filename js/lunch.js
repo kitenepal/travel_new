806bd7b7d2d30a17ddd4be672d3161bc5f89692e
NidanStha <nidanshrestha@gmail.com>
2018-05-20 14:08:14 +0545

    nav collapse

--- js/lunch.js
+++ js/lunch.js
@@ -3,9 +3,35 @@ $(function(){
 });
 $(window).scroll( () => {
 	var windowTop = $(window).scrollTop();
-	windowTop > 70 ? $('.luncher').show() : $('.luncher').hide();
+	windowTop > 100 ? $('.luncher').show() : $('.luncher').hide();
 });
 $('.luncher').on('click', function(e){
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
+/*
+$(function(){
+  var panalID= $(this).attr('data-panelid');
+
+  $('.show-menu').on('click',function(){
+    $('#'+panalID).slideDown();
+  }, function(){
+      $('#'+panalID).slideUp();
+    });
+});
+
+$(function(){
+  var panalID= $(this).attr('data-panelid');
+
+  $('.show-menu').on('click',function(){
+    $('#'+panalID).slideDown();
+  }, function(){
+      $('#'+panalID).slideUp();
+    });
+});
+
+$('.show-menu').toggle(function(){
+	$('#panel1').show();
+}, function(){
+	$('#panel1').hide();
+});*/
